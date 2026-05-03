a6cb8d1330e931a201fd84cd75444a4858de3567
DaFum <150466734+DaFum@users.noreply.github.com>
2026-05-03 22:44:31 +0200

    Delete fix_imageGen.sh

--- fix_imageGen.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-cat tests/node/imageGen.test.js | grep "isImageGenerationAvailable returns true" -A 30
-echo "---"
-cat tests/node/imageGen.test.js | grep "fetchGenImageAsObjectUrl returns offline fallback" -A 40
