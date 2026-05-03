52f9e8982a69164ba997ee4c6ea77268917b477a
DaFum <150466734+DaFum@users.noreply.github.com>
2026-05-03 22:43:09 +0200

    Delete debug_path.js

--- debug_path.js
+++
@@ -1,6 +0,0 @@
-import { getMessyPath } from './src/scenes/kabelsalat/kabelsalatUtils.ts';
-
-const MOCK_SLOT_XS = [100, 200, 300, 400, 500];
-
-const path = getMessyPath('iec', 'mic', ['power', 'mic', 'amp']);
-console.log(path);
